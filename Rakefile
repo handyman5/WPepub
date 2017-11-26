44b0741b0654caf475fbb84358f0fd55b1d5cf97
Adam Compton <acompton@quantcast.com>
2017-11-25 18:42:08 -0800

    Checking in the working set that can pull down a different story

--- Rakefile
+++ Rakefile
@@ -1,18 +1,20 @@
-file "build/out.epub" => FileList["rst/01.01_Gestation_1.1.rst"] do
+file "build/out.epub" => FileList["rst/Prologue_Prologue.rst"] do
 	mkdir_p "build"
-	sh 'echo "Worm.\n=====\n\nparahumans.wordpress.com\n------------------------\n" > rst/0.0_Title.rst'
-	sh "txt2epub build/out.epub rst/*.rst --title=Worm --creator=parahumans.wordpress.com"
+	sh 'echo "Practical Guide to Evil.\n========================\n\practicalguidetoevil.wordpress.com\n------------------------\n" > rst/0.0_Title.rst'
+	sh "txt2epub build/out.epub rst/*.rst --title='Practical Guide to Evil' --creator=practicalguidetoevil.wordpress.com"
 end
 
 
-mirror_fname = "mirror/category/stories-arcs-1-10/arc-1-gestation/1-01/index.html"
+#mirror_fname = "mirror/category/stories-arcs-1-10/arc-1-gestation/1-01/index.html"
+mirror_fname = "mirror/2017/09/04/chapter-28-gambits/index.html"
+
 file mirror_fname do
 	Dir.chdir("WPepub") do
 		sh "python scrape.py"
 	end
 end
 
-file "rst/01.01_Gestation_1.1.rst" => [mirror_fname] do
+file "rst/Prologue_Prologue.rst" => [mirror_fname] do
 	Dir.chdir("WPepub") do
 		sh "python convert.py"
 	end
--- WPepub/convert.py
+++ WPepub/convert.py
@@ -5,12 +5,15 @@ from utils import html2rst, Chapter
 
 def WP_convert():
     # Pull down a single episode in order to extract the TOC
-    with open("../mirror/category/stories-arcs-1-10/arc-1-gestation/1-01/index.html", 'r') as f:
+    #with open("../mirror/category/stories-arcs-1-10/arc-1-gestation/1-01/index.html", 'r') as f:
+    with open("../mirror/2017/09/04/chapter-28-gambits/index.html", 'r') as f:
+
         page_content = f.read()
 
-    soup = BeautifulSoup(page_content)
+    soup = BeautifulSoup(page_content, 'html.parser')
 
-    categories = soup.find_all(attrs={"class":"widget_categories"})[0]
+    categories = soup.find_all(attrs={"class":"menu-item-1416"})[0]
+    #categories = soup.find_all(attrs={"class":"widget_categories"})[0]
     groups = categories.contents[2]
 
     lis = groups.find_all("li")
@@ -19,21 +22,32 @@ def WP_convert():
         if li.li is None:
             print li.a.contents[0], li.a.get('href')
             url = li.a.get('href')
-            mirror_path = os.path.join(url.replace('https://parahumans.wordpress.com', '../mirror'), 'index.html')
-            mirror_path = mirror_path.encode('ascii', 'ignore')
+            mirror_path = os.path.join(url.replace('https://practicalguidetoevil.wordpress.com', '../mirror'), 'index.html')
+            #mirror_path = os.path.join(url.replace('https://parahumans.wordpress.com', '../mirror'), 'index.html')
+            mirror_path = unidecode(mirror_path.decode('utf-8'))
             if 'arc-29' in mirror_path:
                 break
             with open(mirror_path, 'r') as f:
-                content = BeautifulSoup(f.read())
+                content = BeautifulSoup(f.read(), 'html.parser')
             chapters.append(Chapter(title=li.a.contents[0], url=url, content=content))
 
+    try:
+        os.mkdir('../rst')
+    except OSError:
+        pass
+
     for c in chapters:
-        display_title = unidecode(c.content.find_all(attrs={'class':'entry-title'})[0].get_text())
+        try:
+            display_title = c.content.find_all(attrs={'class':'entry-title'})[0].get_text().encode('ascii', 'ignore')
+        except Exception:
+            display_title = "Undefined"
         fname = c.title + ' ' + display_title
         fname = fname.replace(' ', '_').replace('/', '-')
         if len(fname.split('.')[0]) == 1:
             fname = '0' + fname
-        f = open('../rst/' + fname + '.rst', 'w')
+        chapter_num = '%03d' % chapters.index(c)
+        fname = fname.encode('utf-8').decode('ascii', 'ignore')
+        f = open('../rst/' + chapter_num + '-' + fname + '.rst', 'w')
         f.write("{}\n{}\n".format(display_title,'='*(len(display_title))))
         text = html2rst(str(c.content.find_all("div", attrs={'class':'entry-content'})[0]))
         text = '\n'.join(text.splitlines()[1:-1])

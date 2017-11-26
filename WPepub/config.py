0f389c7d2b6138e0cc7b82ef34b484ce5022b8d9
Your Correct Name <comptona@gmail.com>
2017-11-25 22:58:49 -0800

    final cleanup

--- WPepub/config.py
+++
@@ -1,5 +0,0 @@
-options = {
-    'first_chapter_url': 'https://practicalguidetoevil.wordpress.com/2015/03/25/prologue/',
-    'categories_selector': 'menu-item-1416',
-    'toc_url': 'https://practicalguidetoevil.wordpress.com/table-of-contents/',
-}
